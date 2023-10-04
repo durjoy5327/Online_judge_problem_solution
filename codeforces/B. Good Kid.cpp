864e5f2eb68a07cd0f827ece07ff6b6bcd978621
Durjoy Barua <durjoybarua8115@example.com>
2023-10-04 23:09:19 +0600

    changed

--- codeforces/B. Good Kid.cpp
+++ codeforces/B. Good Kid.cpp
@@ -1,35 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
+int main() {
     int t;
-    cin >> t;
-    while (t--)
-    {
-        int n, min = 10, sum = 1, minI = 0;
-        cin >> n;
-        vector<int> a;
-        while (n--)
-        {
-            int x;
-            cin >> x;
-            a.push_back(x);
-        }
-
-        for (int i = 0; i < a.size(); i++)
-        {
-            if (min > a[i])
-            {
-                min = a[i];
-                minI = i;
+    cin>>t;
+    while(t--){
+        int n,min=INT_MAX,k=-1;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            if(min>=a[i]){
+                min=a[i];
+                k=i;
             }
         }
-
-        a[minI]++;
-        for (int i = 0; i < a.size(); i++)
-        {
-            sum *= a[i];
+        a[k]=min+1;
+        int sum=1;
+        for(int i=0;i<n;i++){
+            sum= sum*a[i];
         }
-        cout << sum << endl;
+        cout<<sum<<endl;
     }
+
+    return 0;
 }
